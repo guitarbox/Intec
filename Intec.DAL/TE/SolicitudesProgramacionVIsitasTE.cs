00fbeecd058f045f06c7635672d47486ecf7bfa9
Wilson Alexánder Laguna Cárdenas <stay_together7@hotmail.com>
2020-09-15 18:02:53 -0500

    Trajimos datos de la bd

---
+++ Intec.DAL/TE/SolicitudesProgramacionVIsitasTE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intec.DAL.TE
+{
+    public class SolicitudesProgramacionVIsitasTE
+    {
+        public List<SolicitudesProgramacionVisitas> ConsultarSolicitudes()
+        {
+            List<SolicitudesProgramacionVisitas> res = new List<SolicitudesProgramacionVisitas>();
+            using (var ctx = new DB_A66D31_intratecPrbEntities())
+            {
+                res = ctx.SolicitudesProgramacionVisitas.Where(s=>s.IdPais == 1).ToList();
+                foreach (SolicitudesProgramacionVisitas sol in res)
+                {
+                    ctx.Entry(sol).Reference(r => r.Paises).Load();
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List < Intec.DAL.SolicitudesProgramacionVisitas > solicitudes = new Intec.DAL.TE.SolicitudesProgramacionVIsitasTE().ConsultarSolicitudes();
+            foreach (Intec.DAL.SolicitudesProgramacionVisitas solicitud in solicitudes)
+            {
+                Console.WriteLine($"{solicitud.Nombre} {solicitud.Paises.CodigoPais}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
