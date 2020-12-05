34067bf535264c612e7a6305af219d266fe5c3f9
Alan Sierra <sierra.alan456@gmail.com>
2020-12-05 12:31:11 -0500

    Continuación Métodos DAL

--- Intec.DAL/TE/ClientesTE.cs
+++ Intec.DAL/TE/ClientesTE.cs
@@ -18,5 +18,82 @@ namespace Intec.DAL.TE
             }
             return res;
         }
+
+        public List<Clientes> ConsultaDetalladaClientes(int IdCliente)
+        {
+            List<Clientes> res = new List<Clientes>();
+            using (var ctx = new DB_A66D31_intratecPrbEntities1())
+            {
+                res = ctx.Clientes.Where(c => c.IdCliente == IdCliente).ToList();
+            }
+            return res;
+        }
+
+        public void EditarClientes()
+        {
+            
+        }
+
+        public void CrearPropiedad(Propiedades PropiedadCrear)
+        {
+            using (var ctx = new DB_A66D31_intratecPrbEntities1())
+            {
+                int us = ctx.Propiedades.Where(u => u.IdPropiedades.Equals(PropiedadCrear.IdPropiedades)).ToList().Count;
+                if (us == 0)
+                {
+                    PropiedadCrear.FechaCreacion = DateTime.Now;
+                    ctx.Propiedades.Add(PropiedadCrear);
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception($"Ya existe una Propiedad con ID {PropiedadCrear.IdPropiedades}");
+                }
+
+            }
+        }
+
+        public void EditarPropiedad()
+        {
+
+        }
+
+        public void EliminarPropiedad(Propiedades PropiedadEliminar)
+        {
+            using (var ctx = new DB_A66D31_intratecPrbEntities1())
+            {
+                int us = ctx.Propiedades.Where(u => u.IdPropiedades.Equals(PropiedadEliminar.IdPropiedades)).ToList().Count;
+                if (us == 1)
+                {
+                    PropiedadEliminar.FechaModificacion = DateTime.Now;
+                    ctx.Propiedades.Remove(PropiedadEliminar);
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception($"No existe una Propiedad con ID {PropiedadEliminar.IdPropiedades}");
+                }
+
+            }
+        }
+
+        public void CrearCliente(Clientes ClienteCrear)
+        {
+            using (var ctx = new DB_A66D31_intratecPrbEntities1())
+            {
+                int us = ctx.Clientes.Where(u => u.NumeroIdentificacion.Equals(ClienteCrear.NumeroIdentificacion)).ToList().Count;
+                if (us == 0)
+                {
+                    ClienteCrear.FechaCreacion = DateTime.Now;
+                    ctx.Clientes.Add(ClienteCrear);
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception($"Ya existe un cliente con Número de Identificación {ClienteCrear.NumeroIdentificacion}");
+                }
+
+            }
+        }
     }
 }
