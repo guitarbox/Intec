f0fc6c08568269f6848619b80d09fd904918742e
Alan Sierra <sierra.alan456@gmail.com>
2020-12-11 14:48:12 -0500

    Inicio Metodos papeleria

--- Intec.DAL/TE/PapeleriaTE.cs
+++ Intec.DAL/TE/PapeleriaTE.cs
@@ -9,8 +9,51 @@ namespace Intec.DAL.TE
     public class PapeleriaTE
     {
         //CRUD Formatos, pero el delete no debe permitir eliminar formatos que hayan sido asignados,
+        public void CrearFormato(Formatos FormatoCrear)
+        {
+            using (var ctx = new DB_A66D31_intratecPrbEntities1())
+            {
+                FormatoCrear.FechaCreacion = DateTime.Now;
+                ctx.Formatos.Add(FormatoCrear);
+                ctx.SaveChanges();
+            }
+        }
+
+        public void EditarFormato(Formatos Formato, int IdUsuarioModificacion)
+        {
+            using (var ctx = new DB_A66D31_intratecPrbEntities1())
+            {
+                Formatos FormatoModificar = ctx.Formatos.Where(c => c.IdFormato == Formato.IdFormato).FirstOrDefault();
+
+                FormatoModificar.Formato = Formato.Formato;
+                FormatoModificar.Mascara = Formato.Mascara;
+                FormatoModificar.Activo = Formato.Activo;
+
+                FormatoModificar.FechaModificacion = DateTime.Now;
+                FormatoModificar.IdUsuarioModificacion = IdUsuarioModificacion;
+
+                ctx.SaveChanges();
+            }
+
+
+          
+        }
+
+        public List<Formatos> ConsultarFormatos()
+        {
+            List<Formatos> res = new List<Formatos>();
+
+            using(var ctx = new DB_A66D31_intratecPrbEntities1())
+            {
+                res = ctx.Formatos.ToList();
+            }
+
+            return res;
+
+        }
 
         //Kárdex, solo tendrá insert
 
+
+
     }
-}
