a9a539dbb1d69f428299c9c7a6b378a753d4bfae
Wilson Alexánder Laguna Cárdenas <stay_together7@hotmail.com>
2021-01-12 16:41:44 -0500

    arranco visitas 2

--- Intec.DAL/TE/VisitasTE.cs
+++ Intec.DAL/TE/VisitasTE.cs
@@ -8,5 +8,18 @@ namespace Intec.DAL.TE
 {
     public class VisitasTE
     {
+        //ZONAS => Todo lo que tenga que ver con zonas
+        //CRUD ZONAS
+        //ASignación de zona a inspector, con manejo del histórico
+        //Reporte zonas y presencia en visitas
+
+        //VISITAS => Todo lo que tenga que ver con visitas
+        //Programación Visita
+        //Reasignación visita
+        //Agregar FotosVisita
+        //Agregar FormatosVisita
+        //Agregar EquiposVisita
+        //Reporte ejecución visitas / Consultar una visita
+        //Consulta de visitas (Varios parámetros)
     }
 }
